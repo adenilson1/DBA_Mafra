80afd6ee494e49dca34f6d701222e98e9169c255
adenilson1 <adenilson99@hotmail.com>
2025-05-10 10:59:33 -0300

    inserções do tipo 1 x 1 e 1 x n

--- cap04/modelagem_banco_de_dados.sql
+++ cap04/modelagem_banco_de_dados.sql
@@ -207,3 +207,161 @@ DESC TELEFONE;
 | NUMERO     | varchar(10)             | YES  |     | NULL    |                |
 | ID_CLIENTE | int                     | YES  | MUL | NULL    |                |
 +------------+-------------------------+------+-----+---------+----------------+
+
+/*
+INFRAESTRUTURA: SERVIÇOS, PASSWORD
+*/
+
+/*INSERINDO DADOS*/
+
+DESC CLIENTE;
++-----------+---------------+------+-----+---------+----------------+
+| Field     | Type          | Null | Key | Default | Extra          |
++-----------+---------------+------+-----+---------+----------------+
+| IDCLIENTE | int           | NO   | PRI | NULL    | auto_increment |
+| NOME      | varchar(30)   | NO   |     | NULL    |                |
+| SEXO      | enum('M','F') | NO   |     | NULL    |                |
+| CPF       | varchar(15)   | YES  | UNI | NULL    |                |
+| EMAIL     | varchar(50)   | YES  | UNI | NULL    |                |
++-----------+---------------+------+-----+---------+----------------+
+
+INSERT INTO CLIENTE(IDCLIENTE, NOME, SEXO, CPF, EMAIL)
+VALUES(NULL, 'JOAO', 'M', '76567587887', 'JOAO@IG.COM');
+
+INSERT INTO CLIENTE(IDCLIENTE, NOME, SEXO, CPF, EMAIL)
+VALUES(NULL, 'CARLOS', 'M', '5464553466', 'CARLOS@IG.COM');
+
+INSERT INTO CLIENTE(IDCLIENTE, NOME, SEXO, CPF, EMAIL)
+VALUES(NULL, 'ANA', 'F', '456545678', 'ANA@IG.COM');
+
+INSERT INTO CLIENTE(IDCLIENTE, NOME, SEXO, CPF, EMAIL)
+VALUES(NULL, 'CLARA', 'F', '5687766856', NULL);
+
+INSERT INTO CLIENTE(IDCLIENTE, NOME, SEXO, CPF, EMAIL)
+VALUES(NULL, 'JORGE', 'M', '8756547688', 'JORGE@IG.COM');
+
+INSERT INTO CLIENTE(IDCLIENTE, NOME, SEXO, CPF, EMAIL)
+VALUES(NULL, 'CELIA', 'F', '5767876889', 'CELIA@IG.COME');
+
+/*INSERT INTO CLIENTE(IDCLIENTE, NOME, SEXO, CPF, EMAIL)
+VALUES(NULL, '', '', '', '');*/
+
+SELECT * FROM CLIENTE;
++-----------+--------+------+-------------+---------------+
+| IDCLIENTE | NOME   | SEXO | CPF         | EMAIL         |
++-----------+--------+------+-------------+---------------+
+|         1 | JOAO   | M    | 76567587887 | JOAO@IG.COM   |
+|         2 | CARLOS | M    | 5464553466  | CARLOS@IG.COM |
+|         3 | ANA    | F    | 456545678   | ANA@IG.COM    |
+|         4 | CLARA  | F    | 5687766856  | NULL          |
+|         5 | JORGE  | M    | 8756547688  | JORGE@IG.COM  |
+|         6 | CELIA  | F    | 5767876889  | CELIA@IG.COME |
++-----------+--------+------+-------------+---------------+
+
+/*INSERÇÕES EM RELACIONAMENTO 1 X 1 */
+
+DESC ENDERECO;
++------------+-------------+------+-----+---------+----------------+
+| Field      | Type        | Null | Key | Default | Extra          |
++------------+-------------+------+-----+---------+----------------+
+| IDENDERECO | int         | NO   | PRI | NULL    | auto_increment |
+| RUA        | varchar(30) | NO   |     | NULL    |                |
+| BAIRRO     | varchar(30) | NO   |     | NULL    |                |
+| CIDADE     | varchar(30) | NO   |     | NULL    |                |
+| ESTADO     | char(2)     | NO   |     | NULL    |                |
+| ID_CLIENTE | int         | YES  | UNI | NULL    |                |
++------------+-------------+------+-----+---------+----------------+
+
+INSERT INTO ENDERECO(IDENDERECO, RUA, BAIRRO, CIDADE, ESTADO, ID_CLIENTE)
+VALUES(NULL, 'RUA ANTONIO SA', 'CENTRO', 'BELO HORIZONTE', 'MG', 4);
+
+INSERT INTO ENDERECO(IDENDERECO, RUA, BAIRRO, CIDADE, ESTADO, ID_CLIENTE)
+VALUES(NULL, 'RUA CAPITAO HERMES', 'CENTRO', 'RIO DE JANEIRO', 'RJ', 1);
+
+INSERT INTO ENDERECO(IDENDERECO, RUA, BAIRRO, CIDADE, ESTADO, ID_CLIENTE)
+VALUES(NULL, 'RUA PRES VARGAS', 'JARDINS', 'SAO PAULO', 'SP', 3);
+
+INSERT INTO ENDERECO(IDENDERECO, RUA, BAIRRO, CIDADE, ESTADO, ID_CLIENTE)
+VALUES(NULL, 'RUA ALFANDEGA', 'ESTACIO', 'RIO DE JANEIRO', 'RJ', 2);
+
+INSERT INTO ENDERECO(IDENDERECO, RUA, BAIRRO, CIDADE, ESTADO, ID_CLIENTE)
+VALUES(NULL, 'RUA DO OUVIDOR', 'FLAMENGO', 'RIO DE JANEIRO', 'RJ', 6);
+
+INSERT INTO ENDERECO(IDENDERECO, RUA, BAIRRO, CIDADE, ESTADO, ID_CLIENTE)
+VALUES(NULL, 'RUA URUGUAIANA', 'CENTRO', 'VITORIA', 'ES', 5);
+
+
+SELECT * FROM ENDERECO;
++------------+--------------------+----------+----------------+--------+------------+
+| IDENDERECO | RUA                | BAIRRO   | CIDADE         | ESTADO | ID_CLIENTE |
++------------+--------------------+----------+----------------+--------+------------+
+|          1 | RUA ANTONIO SA     | CENTRO   | BELO HORIZONTE | MG     |          4 |
+|          2 | RUA CAPITAO HERMES | CENTRO   | RIO DE JANEIRO | RJ     |          1 |
+|          3 | RUA PRES VARGAS    | JARDINS  | SAO PAULO      | SP     |          3 |
+|          4 | RUA ALFANDEGA      | ESTACIO  | RIO DE JANEIRO | RJ     |          2 |
+|          5 | RUA DO OUVIDOR     | FLAMENGO | RIO DE JANEIRO | RJ     |          6 |
+|          6 | RUA URUGUAIANA     | CENTRO   | VITORIA        | ES     |          5 |
++------------+--------------------+----------+----------------+--------+------------+
+/* NOTA: A FK NAO SE REPETE*/
+
+
+/*INSERÇÕES 1 X N*/
+
+DESC TELEFONE;
++------------+-------------------------+------+-----+---------+----------------+
+| Field      | Type                    | Null | Key | Default | Extra          |
++------------+-------------------------+------+-----+---------+----------------+
+| IDTELEFONE | int                     | NO   | PRI | NULL    | auto_increment |
+| TIPO       | enum('RES','CEL','COM') | YES  |     | NULL    |                |
+| NUMERO     | varchar(10)             | YES  |     | NULL    |                |
+| ID_CLIENTE | int                     | YES  | MUL | NULL    |                |
++------------+-------------------------+------+-----+---------+----------------+
+
+INSERT INTO TELEFONE(IDTELEFONE, TIPO, NUMERO, ID_CLIENTE)
+VALUES (NULL, 'CEL', '78458743', 5);
+
+INSERT INTO TELEFONE(IDTELEFONE, TIPO, NUMERO, ID_CLIENTE)
+VALUES (NULL, 'RES', '56576876', 5);
+
+INSERT INTO TELEFONE(IDTELEFONE, TIPO, NUMERO, ID_CLIENTE)
+VALUES (NULL, 'COM', '87866896', 1);
+
+INSERT INTO TELEFONE(IDTELEFONE, TIPO, NUMERO, ID_CLIENTE)
+VALUES (NULL, 'RES', '54768899', 2);
+
+INSERT INTO TELEFONE(IDTELEFONE, TIPO, NUMERO, ID_CLIENTE)
+VALUES (NULL, 'CEL', '99667587', 1);
+
+INSERT INTO TELEFONE(IDTELEFONE, TIPO, NUMERO, ID_CLIENTE)
+VALUES (NULL, 'CEL', '78989765', 3);
+
+INSERT INTO TELEFONE(IDTELEFONE, TIPO, NUMERO, ID_CLIENTE)
+VALUES (NULL, 'CEL', '99766676', 3);
+
+INSERT INTO TELEFONE(IDTELEFONE, TIPO, NUMERO, ID_CLIENTE)
+VALUES (NULL, 'COM', '66687899', 1);
+
+INSERT INTO TELEFONE(IDTELEFONE, TIPO, NUMERO, ID_CLIENTE)
+VALUES (NULL, 'RES', '89986668', 5);
+
+INSERT INTO TELEFONE(IDTELEFONE, TIPO, NUMERO, ID_CLIENTE)
+VALUES (NULL, 'CEL', '88687909', 2);
+
+SELECT * FROM TELEFONE;
++------------+------+----------+------------+
+| IDTELEFONE | TIPO | NUMERO   | ID_CLIENTE |
++------------+------+----------+------------+
+|          1 | CEL  | 78458743 |          5 |
+|          2 | RES  | 56576876 |          5 |
+|          3 | COM  | 87866896 |          1 |
+|          4 | RES  | 54768899 |          2 |
+|          5 | CEL  | 99667587 |          1 |
+|          6 | CEL  | 78989765 |          3 |
+|          7 | CEL  | 99766676 |          3 |
+|          8 | COM  | 66687899 |          1 |
+|          9 | RES  | 89986668 |          5 |
+|         10 | CEL  | 88687909 |          2 |
++------------+------+----------+------------+
+
+
+/*NOTA: A PARTE N DO RELACIONAMENTO PODE SE REPETIR*/
